a53e154f81293cfc12e0dbf9bac9bd65a045cbf0
kywnz <125796060+kywnz@users.noreply.github.com>
2026-01-30 23:33:03 +0700

    Delete bai1.cpp

--- bai1.cpp
+++
@@ -1,8 +0,0 @@
-//nhap vao dien tich S tinh the tich V
-#include <bits/stdc++.h>
-int main(){
-	double S,V,R;
-	cout<<"Nhap vao dien tich S: "; cin>>S;
-	R=sqrt(S/(4*3.14)); V=(4.0/3) * 3.14 *R*R*R;
-	cout<<"The tich: "<<V;
-}
