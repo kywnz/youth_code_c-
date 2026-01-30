434ffcbdd0c64a6af8d7bdbc8f3c7bc1044069d9
kywnz <125796060+kywnz@users.noreply.github.com>
2026-01-30 23:33:32 +0700

    Delete bai3.cpp

--- bai3.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-int main(){
-	double xc,yc,R,xm,ym,d;
-	cin>>xc>>yc>>R>>xm>>ym;
-	d=sqrt(pow((xm-xc),2)+pow((ym-yc),2));
-	if(d<R) cout<<"M nam trong duong tron";
-	else if(d==R) cout<<"M nam tren duong tron";
-	else cout<<"M nam ngoai duong tron";
-}
