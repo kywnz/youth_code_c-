ded069e7ead7f1d737efeb1481cbf47f2090b893
kywnz <125796060+kywnz@users.noreply.github.com>
2026-01-30 23:33:13 +0700

    Delete bai2.cpp

--- bai2.cpp
+++
@@ -1,8 +0,0 @@
-//nhap toa do 2 diem A, B tinh khoang cach
-#include <bits/stdc++.h>
-int main(){
-	double xa,ya,xb,yb,d;
-	cin>>xa>>ya>>xb>>yb;
-	d=sqrt(pow((xb-xa),2)+pow((yb-ya),2));
-	cout<<d;
-}
